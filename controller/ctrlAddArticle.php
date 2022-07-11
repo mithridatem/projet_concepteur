34182a0c8aa4babe02b2a37d7c70e1b1ae940947
mithridatem <mathieumithridate@adrar-formation.com>
2022-07-11 14:52:27 +0200

    add article  update

---
+++ controller/ctrlAddArticle.php
@@ -0,0 +1,28 @@
+<?php
+    $date = date("Y-m-d");
+    include '../view/viewAddArticle.php';
+    //test si submit à été cliqué
+    if(isset($_POST['bt'])){
+        //test si les champs sont remplis
+        if(!empty($_POST['name_art']) AND !empty($_POST['content_art']) ){
+            //test si date vide
+            if(empty($_POST['date_art'])){
+                $date_art = $date;
+                echo "article : ".$_POST['name_art']. " date : " .$date_art; 
+            }
+            //si l'utilisateur à choisi une date
+            else{
+                $date_art = $_POST['date_art'];
+                echo "article : ".$_POST['name_art']. " date : " .$date_art; 
+            }
+        }
+        //test si les champs ne sont pas remplis
+        else{
+            echo "veuillez remplir tous les champs de formulaire";
+        } 
+    }
+    //si le bouton Envoyer n'est pas cliqué
+    else{
+        echo "veuillez compléter le formulaire";
+    }
+?>
--- view/viewAddArticle.php
+++ view/viewAddArticle.php
@@ -3,15 +3,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ajouter un utilisateur</title>
+    <title>Ajouter un article</title>
 </head>
 <body>
-    <h3>Ajouter un utilisateur :</h3>
+    <h3>Ajouter un article :</h3>
     <form action="" method="post">
-        <p><input type="text" name="name_util"></p>
-        <p><input type="text" name="first_name_util"></p>
-        <p><input type="text" name="mail_util"></p>
-        <p><input type="text" name="mdp_util"></p>
+        <p><input type="text" name="name_art"></p>
+        <p><input type="text" name="content_art"></p>
+        <p><input type="date" name="date_art"></p>
         <p><input type="submit" value="Envoyer" name="bt"></p>
     </form>
 </body>
