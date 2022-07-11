d1c3cce4a6f6c00ef909bca1255437f3a6b4b50f
MrMimi11 <remi.prat@laposte.net>
2022-07-11 14:22:15 +0200

    [FEA] add code of Mathieu branch

--- controller/ctrlAdduser.php
+++ controller/ctrlAdduser.php
@@ -1,8 +1,28 @@
 <?php
-if(isset($_POST['submit_util'])){
-    echo $_POST['first_name_util'];
-    echo $_POST['name_util'];
-    echo $_POST['mail_util'];
-    echo $_POST['pwd_util'];
+//if(isset($_POST['submit_util'])){
+//    echo $_POST['first_name_util'];
+//    echo $_POST['name_util'];
+//    echo $_POST['mail_util'];
+//    echo $_POST['pwd_util'];
+//    echo "l\utilisateur a été ajouté:"echo $_POST['name_util'];
+//}
+//?>
+
+<?php
+//imports
+//test vérifier le contenu des inputs
+//test si submit à été cliqué
+if(isset($_POST['bt'])){
+    //test si les champs sont remplis
+    if(!empty($_POST['name_util']) AND !empty($_POST['first_name_util']) AND
+        !empty($_POST['mail_util']) AND !empty($_POST['mdp_util'])){
+        echo "l'utilisateur suivant à été ajouté : ".$_POST['name_util']. " ". $_POST['first_name_util'];
+    }
+    else{
+        echo "veuillez remplir tous les champs de formulaire";
+    }
+}
+else{
+    echo "veuillez compléter le formulaire";
 }
 ?>
--- view/viewAddUser.php
+++ view/viewAddUser.php
@@ -1,20 +1,42 @@
-<!DOCTYPE html>
-<html>
+<!--<!DOCTYPE html>-->
+<!--<html>-->
+<!--<head>-->
+<!--    <title>Ajouter Utilisateur</title>-->
+<!--    <link href="../css/add_user.css" rel="stylesheet" type="text/css">-->
+<!--</head>-->
+<!--<body>-->
+<!--<form action="../controller/ctrlAdduser.php" method="post">-->
+<!--    <p>Prénom:</p>-->
+<!--    <input type="text" name="first_name_util" />-->
+<!--    <p>Nom:</p>-->
+<!--    <input type="text" name="name_util" />-->
+<!--    <p>Email:</p>-->
+<!--    <input type="email" name="mail_util" />-->
+<!--    <p>Password:</p>-->
+<!--    <input type="password" name="pwd_util" />-->
+<!--    <input type="submit" value="Valider" name="submit_util" id="submit" />-->
+<!--</form>-->
+<!--</body>-->
+<!--</html>-->
+
+<html lang="en">
 <head>
-    <title>Ajouter Utilisateur</title>
-    <link href="../css/add_user.css" rel="stylesheet" type="text/css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajouter un utilisateur</title>
 </head>
 <body>
-<form action="../controller/ctrlAdduser.php" method="post">
-    <p>Prénom:</p>
-    <input type="text" name="first_name_util" />
-    <p>Nom:</p>
-    <input type="text" name="name_util" />
-    <p>Email:</p>
-    <input type="email" name="mail_util" />
+<form action="../controller/ctrlAddUser.php" method="post">
+    <p>Name:</p>
+    <p><input type="text" name="name_util"></p>
+    <p>First Name:</p>
+    <p><input type="text" name="first_name_util"></p>
+    <p>Mail:</p>
+    <p><input type="text" name="mail_util"></p>
     <p>Password:</p>
-    <input type="password" name="pwd_util" />
-    <input type="submit" value="Valider" name="submit_util" id="submit" />
+    <p><input type="password" name="mdp_util"></p>
+    <p><input type="submit" value="Envoyer" name="bt"></p>
 </form>
 </body>
 </html>
