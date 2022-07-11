9dc81bede22bfb378b3c7da328bfb5796da5c4e6
MrMimi11 <remi.prat@laposte.net>
2022-07-11 15:10:11 +0200

    [FEA] add code of Mathieu branch article and add select for view article

--- view/viewAddArticle.php
+++ view/viewAddArticle.php
@@ -1,18 +1,46 @@
-<!DOCTYPE html>
-<html>
+<!--<!DOCTYPE html>-->
+<!--<html>-->
+<!--<head>-->
+<!--    <title>Ajouter article</title>-->
+<!--    <link href="../css/adduser.css" rel="stylesheet" type="text/css">-->
+<!--</head>-->
+<!--<body>-->
+<!--<form action="../controller/ctrlAddArticle.php" method="post">-->
+<!--    <p>Titre:</p>-->
+<!--    <input type="text" name="name_art"/>-->
+<!--    <p>Contenu:</p>-->
+<!--    <textarea name="content_art"></textarea>-->
+<!--    <p>Date:</p>-->
+<!--    <input type="date" name="date_art"/>-->
+<!--    <input type="submit" value="Valider" name="submit_util" id="submit"/>-->
+<!--</form>-->
+<!--</body>-->
+<!--</html>-->
+
+<html lang="en">
 <head>
-    <title>Ajouter article</title>
-    <link href="../css/adduser.css" rel="stylesheet" type="text/css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajouter un article</title>
 </head>
 <body>
-<form action="../controller/ctrlAddArticle.php" method="post">
+<h3>Ajouter un article :</h3>
+<form action="" method="post">
     <p>Titre:</p>
-    <input type="text" name="name_art"/>
+    <p><input type="text" name="name_art"></p>
     <p>Contenu:</p>
-    <textarea name="content_art"></textarea>
+    <p><input type="text" name="content_art"></p>
     <p>Date:</p>
-    <input type="date" name="date_art"/>
-    <input type="submit" value="Valider" name="submit_util" id="submit"/>
+    <p><input type="date" name="date_art"></p>
+    <label for="categoryselect">Catégorie:</label>
+
+    <select name="category" id="categoryselect">
+        <option value="news">News</option>
+        <option value="info">Info</option>
+        <option value="event">Evènement</option>
+    </select>
+    <p><input type="submit" value="Envoyer" name="bt"></p>
 </form>
 </body>
 </html>
