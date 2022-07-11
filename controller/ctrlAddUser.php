7ff314e1cfc182620bb36987536a88822a1f105a
mithridatem <mathieumithridate@adrar-formation.com>
2022-07-11 15:39:32 +0200

    bdd

--- controller/ctrlAddUser.php
+++ controller/ctrlAddUser.php
@@ -1,12 +1,15 @@
 <?php
 //imports
 include '../view/viewAddUser.php';
+include '../utils/connectBdd.php';
+
 //test vérifier le contenu des inputs
 //test si submit à été cliqué
 if(isset($_POST['bt'])){
     //test si les champs sont remplis
     if(!empty($_POST['name_util']) AND !empty($_POST['first_name_util']) AND 
     !empty($_POST['mail_util']) AND !empty($_POST['mdp_util'])){
+        addUser($bdd);
         echo "l'utilisateur suivant à été ajouté : ".$_POST['name_util']. " ". $_POST['first_name_util'];
     }
     else{
@@ -15,5 +18,25 @@ if(isset($_POST['bt'])){
 }
 else{
     echo "veuillez compléter le formulaire";
+}
+
+
+//fonctions 
+function addUser($bdd){
+    try{
+        $req = $bdd->prepare("insert into utilisateur(name_util, first_name_util, mail_util, mdp_util) values
+        (:name_util, :first_name_util, :mail_util, :mdp_util)");
+        $req->execute(array(
+            'name_util' => $_POST['name_util'],
+            'first_name_util' => $_POST['first_name_util'],
+            'mail_util' => $_POST['mail_util'],
+            'mdp_util' => password_hash($_POST['mdp_util'], PASSWORD_DEFAULT),
+        ));
+    }
+    catch(Exception $e)
+    {
+        //affichage d'une exception en cas d’erreur
+        die('Erreur : '.$e->getMessage());
+    }
 }
 ?>
