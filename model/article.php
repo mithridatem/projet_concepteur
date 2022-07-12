a21d01e411c9d01e0aee31e8bd23c0223a6d7f7a
MrMimi11 <remi.prat@laposte.net>
2022-07-12 16:43:37 +0200

    [FEA] add class for article and utilisateur in model

---
+++ model/article.php
@@ -0,0 +1,44 @@
+<?php
+class Article{
+    //attributs
+    private $id_art;
+    private $name_art;
+    private $content_art;
+    private $date_art;
+    private $id_type;
+    //constructeur
+    public function __construct(){
+    }
+    //getter and setter
+    public function getIdArt():int{
+        return $this->id_art;
+    }
+    public function getNameArt():string{
+        return $this->name_art;
+    }
+    public function getContentArt():string{
+        return $this->content_art;
+    }
+    public function getDateArt():string{
+        return $this->date_art;
+    }
+    public function getIdType():int{
+        return $this->id_role;
+    }
+    public function setIdArt($id):int{
+        return $this->id_art;
+    }
+    public function setNameArt($name){
+        $this->name_art = $name;
+    }
+    public function setContentArt($content){
+        $this->content_art = $content;
+    }
+    public function setDateArt($date){
+        $this->date_art = $date;
+    }
+    public function setIdType($id){
+        $this->id_type = $id;
+    }
+}
+?>
--- model/utilisateur.php
+++ model/utilisateur.php
@@ -1,21 +1,75 @@
 <?php
-//fonctions
-function addUser($bdd, $file){
-    try{
-        $req = $bdd->prepare("insert into utilisateur(name_util, first_name_util, mail_util, mdp_util, img_util) values
-            (:name_util, :first_name_util, :mail_util, :mdp_util, :img_util)");
-        $req->execute(array(
-            'name_util' => $_POST['name_util'],
-            'first_name_util' => $_POST['first_name_util'],
-            'mail_util' => $_POST['mail_util'],
-            'mdp_util' => password_hash($_POST['mdp_util'], PASSWORD_DEFAULT),
-            'img_util' => $file
-        ));
+class utilisateur{
+    //attributs
+    private $id_util;
+    private $name_util;
+    private $first_name_util;
+    private $mail_util;
+    private $mdp_util;
+    private $img_util;
+    private $statut_util;
+    private $id_role;
+    //constructeur
+    public function __construct(){
     }
-    catch(Exception $e)
+
+    //getter and setter
+    public function getIdUtil():int{
+        return $this->id_util;
+    }
+    public function getNameUtil()
     {
-        //affichage d'une exception en cas d’erreur
-        die('Erreur : '.$e->getMessage());
+        return $this->name_util;
+    }
+    public function getFirstNameUtil()
+    {
+        return $this->first_name_util;
+    }
+    public function getMailUtil()
+    {
+        return $this->mail_util;
+    }
+    public function setIdUtil($id):int{
+        return $this->id_util;
+    }
+    public function setNameUtil($name_util): void
+    {
+        $this->name_util = $name_util;
+    }
+    public function setFirstNameUtil($first_name_util): void
+    {
+        $this->first_name_util = $first_name_util;
+    }
+    public function setMailUtil($mail_util): void
+    {
+        $this->mail_util = $mail_util;
+    }
+    public function getMdpUtil()
+    {
+        return $this->mdp_util;
+    }
+    public function setMdpUtil($mdp_util): void
+    {
+        $this->mdp_util = $mdp_util;
+    }
+    public function getImgUtil()
+    {
+        return $this->img_util;
+    }
+    public function setImgUtil($img_util): void
+    {
+        $this->img_util = $img_util;
+    }
+    public function getStatutUtil()
+    {
+        return $this->statut_util;
+    }
+    public function setStatutUtil($statut_util): void
+    {
+        $this->statut_util = $statut_util;
+    }
+    public function setIdRole($id){
+        $this->id_role = $id;
     }
 }
 ?>
