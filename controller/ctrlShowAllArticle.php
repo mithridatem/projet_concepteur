8c15915a5a5aa733d995732347c4466c1632fa13
mithridatem <mathieumithridate@adrar-formation.com>
2022-07-12 11:58:38 +0200

    update

---
+++ controller/ctrlShowAllArticle.php
@@ -0,0 +1,10 @@
+<?php
+    //import
+    include '../view/viewAllArticle.php';
+    include '../utils/connectBdd.php';
+    include '../model/article.php';
+    //boucle affichage de la liste des articles
+    foreach(getAllArticle($bdd) as $value){
+        $value->name_art."<br>";
+    }
+?>
