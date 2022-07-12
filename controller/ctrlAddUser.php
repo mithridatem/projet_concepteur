8e2c904e5f2ea677488c163762052ca54e25024e
Guanod <joeldev.eu@gmail.com>
2022-07-12 10:10:15 +0200

    changes

--- controller/ctrlAddUser.php
+++ controller/ctrlAddUser.php
@@ -1,47 +1,49 @@
-
 <?php
-include '../view/viewAddUser.php';#
-include '../utils/connectBdd.php';
+    //imports
+    include '../view/viewAddUser.php';
+    include '../utils/connectBdd.php';
+    include '../model/utilisateur.php';
+    $file = "defaut.jpg";
+    //test vérifier le contenu des inputs
+    //test si submit à été cliqué
+    if(isset($_POST['bt'])){
+        //test si les champs sont remplis
+        if(!empty($_POST['name_util']) AND !empty($_POST['first_name_util']) AND 
+        !empty($_POST['mail_util']) AND !empty($_POST['mdp_util'])){
+            if($_FILES['img_util']['name']!=""){
+                //stocke le chemin et le nom temporaire du fichier importé (ex /tmp/125423.pdf)
+                $tmpName = $_FILES['img_util']['tmp_name'];
+                //stocke le nom du fichier (nom du fichier et son extension importé ex : test.jpg)
+                $name = $_FILES['img_util']['name'];
+                //stocke la taille du fichier en octets
+                $size = $_FILES['img_util']['size'];
+                //stocke les erreurs (pb d'import, pb de droits etc…)
+                $error = $_FILES['img_util']['error'];
+                //stocker le chemin de l'image 
+                $file = "../asset/images/$name";
+                //déplacer le fichier importé dans le dossier image à la racine du projet
+                $fichier = move_uploaded_file($tmpName, $file);
+                echo "test";
+                addUser($bdd,$name);
+            }
+            else{
+                addUser($bdd,$file);
+            }
+            
 
-
-if(isset($_POST['submit'])){
-
-
-  echo'<pre>';
-  var_dump($_POST);
-  var_dump($_FILES);
-  var_dump($_POST['img_util']);
-  echo'<pre>';
-
-    // test si les champs sont remplis
-    if(!empty($_POST['name_util']) AND !empty($_POST['first_name_util']) AND 
-    !empty($_POST['mail_util']) AND !empty($_POST['mdp_util'])){
-        echo "l'utilisateur suivant à été ajouté : ".$_POST['name_util']. " ". $_POST['first_name_util'];
-        addUser($bdd );
+            echo "l'utilisateur suivant à été ajouté : ".$_POST['name_util']. " ". $_POST['first_name_util'];
+        }
+        else{
+            echo "veuillez remplir tous les champs de formulaire";
+        } 
     }
     else{
-        echo "veuillez remplir tous les champs de formulaire";
-    } 
-}
-else{
-    echo "veuillez compléter le formulaire";
-}
+        echo "veuillez compléter le formulaire";
+    }
 
-function addUser($bdd){
-    try{
-        $req = $bdd->prepare("insert into utilisateur(name_util, first_name_util, mail_util, mdp_util) values
-        (:name_util, :first_name_util, :mail_util, :mdp_util)");
-        $req->execute(array(
-            'name_util' => $_POST['name_util'],
-            'first_name_util' => $_POST['first_name_util'],
-            'mail_util' => $_POST['mail_util'],
-            'mdp_util' => password_hash($_POST['mdp_util'], PASSWORD_DEFAULT),
-        ));
-    }
-    catch(Exception $e)
-    {
-        //affichage d'une exception en cas d’erreur
-        die('Erreur : '.$e->getMessage());
-    }
-}
+    $tab = getAllUser($bdd);
+    foreach($tab as $value){
+        echo $value->name_util."<br>";
+    } 
+    
 ?>
--- model/utilisateur.php
+++ model/utilisateur.php
@@ -1,46 +1,101 @@
 <?php
-    class Utilisateur{
+    // class Utilisateur{
         /*--------------------------------------------------
                             Attributs
         --------------------------------------------------*/
-        private $id_util;
-        private $name_util;
-        private $first_name_util;
-        private $mail_util;
-        private $mdp_util;
-        private $img_util;
-        private $statut_util;
-        private $id_role;
-        /*--------------------------------------------------
-                        Getters And Setters
-        --------------------------------------------------*/
-        //GETTERS
-        public function getIdUtil(){
-            return $this->id_util;
-        }
-        public function getNameUtil(){
-            return $this->name_util;
-        }
-        public function getFirstNameUtil(){
-            return $this->first_name_util;
-        }
-        public function getMailUtil(){
-            return $this->mail_util;
-        }
-        public function getMdpUtil(){
-            return $this->mdp_util;
-        }
-        public function getImgUtil(){
-            return $this->id_util;
-        }
-        public function getStatutUtil(){
-            return $this->statut_util;
-        }
-        public function getIdRole(){
-            return $this->id_role;
-        }
+    //     private $id_util;
+    //     private $name_util;
+    //     private $first_name_util;
+    //     private $mail_util;
+    //     private $mdp_util;
+    //     private $img_util;
+    //     private $statut_util;
+    //     private $id_role;
+    //     /*--------------------------------------------------
+    //                     Getters And Setters
+    //     --------------------------------------------------*/
+    //     //GETTERS
+    //     public function getIdUtil(){
+    //         return $this->id_util;
+    //     }
+    //     public function getNameUtil(){
+    //         return $this->name_util;
+    //     }
+    //     public function getFirstNameUtil(){
+    //         return $this->first_name_util;
+    //     }
+    //     public function getMailUtil(){
+    //         return $this->mail_util;
+    //     }
+    //     public function getMdpUtil(){
+    //         return $this->mdp_util;
+    //     }
+    //     public function getImgUtil(){
+    //         return $this->id_util;
+    //     }
+    //     public function getStatutUtil(){
+    //         return $this->statut_util;
+    //     }
+    //     public function getIdRole(){
+    //         return $this->id_role;
+    //     }
         
         
-    }
+    // }
 
+?>
+
+<?php
+     //fonctions 
+     function addUser($bdd, $file){
+        try{
+            $req = $bdd->prepare("insert into utilisateur(name_util, first_name_util, mail_util, mdp_util, img_util) values
+            (:name_util, :first_name_util, :mail_util, :mdp_util, :img_util)");
+            $req->execute(array(
+                'name_util' => $_POST['name_util'],
+                'first_name_util' => $_POST['first_name_util'],
+                'mail_util' => $_POST['mail_util'],
+                'mdp_util' => password_hash($_POST['mdp_util'], PASSWORD_DEFAULT),
+                'img_util' => $file
+            ));
+        }
+        catch(Exception $e)
+        {
+            //affichage d'une exception en cas d’erreur
+            die('Erreur : '.$e->getMessage());
+        }
+    }
+    function getAllUserbyMail($bdd):bool{
+        try{
+            $req = $bdd->prepare("SELECT * FROM utilisateur where mail_util =:mail_util limit");
+            $req->execute(array(
+                'mail_util'=>$_POST['mail_util'],
+            ));
+            $data = $req->fetchAll(PDO::FETCH_OBJ);
+            if(isset($data)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        catch(Exception $e)
+        {
+            //affichage d'une exception en cas d’erreur
+            die('Erreur : '.$e->getMessage());
+        }
+    } 
+    function getAllUser($bdd):array{
+        try{
+            $req = $bdd->prepare("SELECT * FROM utilisateur");
+            $req->execute();
+            $data = $req->fetchAll(PDO::FETCH_OBJ);
+            return $data;
+        }
+        catch(Exception $e)
+        {
+            //affichage d'une exception en cas d’erreur
+            die('Erreur : '.$e->getMessage());
+        }
+    }       
 ?>
