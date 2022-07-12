e7a62a6955db40982499f74cacf67dc5f400b5cf
MrMimi11 <remi.prat@laposte.net>
2022-07-12 16:40:21 +0200

    [FEA] update controller and add showallarticle controller

--- controller/ctrlAddArticle.php
+++ controller/ctrlAddArticle.php
@@ -8,20 +8,28 @@
 
 <?php
 $date = date("Y-m-d");
-include '../view/viewAddArticle.php';
+include './view/viewAddArticle.php';
+include './utils/connectBdd.php';
+include './model/article.php';
+include './manager/article.php';
 //test si submit à été cliqué
 if(isset($_POST['bt'])){
     //test si les champs sont remplis
     if(!empty($_POST['name_art']) AND !empty($_POST['content_art']) ){
+        $art = new ManagerArticle();
+        $art->setNameArt($_POST['name_art']);
+        $art->setcontentArt($_POST['content_art']);
         //test si date vide
         if(empty($_POST['date_art'])){
             $date_art = $date;
-            echo "article : ".$_POST['name_art']. " date : " .$date_art;
+            $art->addArticle($bdd, $date_art);
+            echo "article : ".$art->getNameArtmArt(). " date : " .$date_art;
         }
         //si l'utilisateur à choisi une date
         else{
             $date_art = $_POST['date_art'];
-            echo "article : ".$_POST['name_art']. " date : " .$date_art;
+            $art->addArticle($bdd, $date_art);
+            echo "article : ".$art->getNameArtmArt(). " date : " .$date_art;
         }
     }
     //test si les champs ne sont pas remplis
--- controller/ctrlAdduser.php
+++ controller/ctrlAdduser.php
@@ -14,31 +14,36 @@ include '../view/viewAddUser.php';
 include '../utils/connectBdd.php';
 include '../model/utilisateur.php';
 
+$file = "defaut.jpg";
 //test vérifier le contenu des inputs
 //test si submit à été cliqué
 if(isset($_POST['bt'])){
     //test si les champs sont remplis
     if(!empty($_POST['name_util']) AND !empty($_POST['first_name_util']) AND
         !empty($_POST['mail_util']) AND !empty($_POST['mdp_util'])){
-        if(isset($_FILES['file'])){
+        if($_FILES['img_util']['name']!=""){
             //stocke le chemin et le nom temporaire du fichier importé (ex /tmp/125423.pdf)
-            $tmpName = $_FILES['file']['tmp_name'];
+            $tmpName = $_FILES['img_util']['tmp_name'];
             //stocke le nom du fichier (nom du fichier et son extension importé ex : test.jpg)
-            $name = $_FILES['file']['name'];
+            $name = $_FILES['img_util']['name'];
             //stocke la taille du fichier en octets
-            $size = $_FILES['file']['size'];
+            $size = $_FILES['img_util']['size'];
             //stocke les erreurs (pb d'import, pb de droits etc…)
-            $error = $_FILES['file']['error'];
+            $error = $_FILES['img_util']['error'];
             //stocker le chemin de l'image
             $file = "../asset/images/$name";
             //déplacer le fichier importé dans le dossier image à la racine du projet
             $fichier = move_uploaded_file($tmpName, $file);
+            echo "test";
+            if(!getAllUserbyMail($bdd)){
+                addUser($bdd,$name);
+            }
 
         }
-        else{
-            $file = "../asset/images/defaut.jpg";
+        if(!getAllUserbyMail($bdd)){
+            addUser($bdd,$file);
         }
-        addUser($bdd,$file);
+
 
         echo "l'utilisateur suivant à été ajouté : ".$_POST['name_util']. " ". $_POST['first_name_util'];
     }
@@ -49,4 +54,10 @@ if(isset($_POST['bt'])){
 else{
     echo "veuillez compléter le formulaire";
 }
+
+$tab = getAllUser($bdd);
+foreach($tab as $value){
+    echo $value->name_util."<br>";
+}
+
 ?>
