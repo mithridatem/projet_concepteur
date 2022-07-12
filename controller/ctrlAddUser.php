481e6a028c6833538150bb4dfb9e0cd5b6afa795
Guanod <joeldev.eu@gmail.com>
2022-07-12 10:41:14 +0200

    articles

--- controller/ctrlAddUser.php
+++ controller/ctrlAddUser.php
@@ -3,34 +3,25 @@
     include '../view/viewAddUser.php';
     include '../utils/connectBdd.php';
     include '../model/utilisateur.php';
-    $file = "defaut.jpg";
+    $file = "default.jpg";
     //test vérifier le contenu des inputs
     //test si submit à été cliqué
-    if(isset($_POST['bt'])){
+    if(isset($_POST['submit'])){
         //test si les champs sont remplis
         if(!empty($_POST['name_util']) AND !empty($_POST['first_name_util']) AND 
         !empty($_POST['mail_util']) AND !empty($_POST['mdp_util'])){
             if($_FILES['img_util']['name']!=""){
-                //stocke le chemin et le nom temporaire du fichier importé (ex /tmp/125423.pdf)
                 $tmpName = $_FILES['img_util']['tmp_name'];
-                //stocke le nom du fichier (nom du fichier et son extension importé ex : test.jpg)
                 $name = $_FILES['img_util']['name'];
-                //stocke la taille du fichier en octets
                 $size = $_FILES['img_util']['size'];
-                //stocke les erreurs (pb d'import, pb de droits etc…)
                 $error = $_FILES['img_util']['error'];
-                //stocker le chemin de l'image 
                 $file = "../asset/images/$name";
-                //déplacer le fichier importé dans le dossier image à la racine du projet
                 $fichier = move_uploaded_file($tmpName, $file);
-                echo "test";
                 addUser($bdd,$name);
             }
             else{
                 addUser($bdd,$file);
             }
-            
-
             echo "l'utilisateur suivant à été ajouté : ".$_POST['name_util']. " ". $_POST['first_name_util'];
         }
         else{
---
+++ model/article.php
@@ -0,0 +1,60 @@
+<?php
+     //fonctions 
+
+     function addArticle($bdd){
+        try{
+            $req = $bdd->prepare("insert into article(name_art, content_art, date_art) values
+            (:name_util, :first_name_util, :mail_util, :mdp_util, :img_util)");
+            $req->execute(array(
+                'name_art' => $_POST['name_art'],
+                'content_art' => $_POST['content_art'],
+                'date_art' => $_POST['date_art'],
+            ));
+        }
+        catch(Exception $e)
+        {
+            //affichage d'une exception en cas d’erreur
+            die('Erreur : '.$e->getMessage());
+        }
+    }
+    
+    function getAllArticle($bdd){
+        try{
+            $req = $bdd->prepare("SELECT * FROM article");
+            $req->execute();
+            $data = $req->fetchAll(PDO::FETCH_OBJ);
+            if(isset($data)){
+                return $data;
+            }
+            else{
+                return false;
+            }
+        }
+        catch(Exception $e)
+        {
+            //affichage d'une exception en cas d’erreur
+            die('Erreur : '.$e->getMessage());
+        }
+    } 
+    
+    function getArticleById($bdd, $id):array{
+        try{
+            $req = $bdd->prepare("SELECT * FROM article WHERE id =:id");
+            $req->execute(array(
+              'id'=>$id
+            ));
+            $data = $req->fetchAll(PDO::FETCH_OBJ);
+            if(isset($data)){
+              return $data;
+          }
+          else{
+              return false;
+          }
+        }
+        catch(Exception $e)
+        {
+            //affichage d'une exception en cas d’erreur
+            die('Erreur : '.$e->getMessage());
+        }
+    }       
+?>
